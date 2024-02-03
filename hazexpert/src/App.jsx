8d89ceff60993f76916d90843de2c931b472e444
Sauhardya27 <sauhardyachakraborty2643@gmail.com>
2024-02-03 12:22:18 +0530

    Navbar finished

--- hazexpert/src/App.jsx
+++ hazexpert/src/App.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import styles from "./style";
 import { Navbar, Hero } from "./components";
 
 const App = () => (
-  <div>App</div>
+  <div className="bg-primary w-full overflow-hidden">
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Navbar />
+      </div>
+    </div>
+
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Hero />
+      </div>
+    </div>
+  </div>
 );
 
 export default App
--- hazexpert/src/components/Navbar.jsx
+++ hazexpert/src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import { close, logo, menu } from '../assets';
 import { navLinks } from '../constants';
 
 const Navbar = () => {
+	const [toggle, setToggle] = useState(false);
 	return (
 		<nav className='w-full flex py-6 justify-between items-center navbar'>
 			<img src={logo} alt="hazexpert" className='w-[124px] h-[32px]' />
