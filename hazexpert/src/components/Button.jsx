ef0c4415a1df904f442f91d0355c4a4510aaaa04
Sauhardya27 <sauhardyachakraborty2643@gmail.com>
2024-02-03 19:20:28 +0530

    Hero updated

--- hazexpert/src/components/Button.jsx
+++ hazexpert/src/components/Button.jsx
@@ -1,8 +1,8 @@
 const Button = () => (
-	<div className="mt-20 space-x-10">
-		<button type="button" className={`bg-gradient-to-r from-purpleG to-whiteG text-white text-base font-semibold px-10 py-2 rounded-full hover:scale-125 ease-out duration-300`}>Dehaze</button>
+	<div className="mt-20 space-x-5 xs:space-x-10">
+		<button type="button" className={`bg-gradient-to-r from-purpleG to-whiteG text-white text-base font-semibold px-5 xs:px-10 py-2 rounded-full hover:scale-125 ease-out duration-300`}>Dehaze</button>
 
-		<button type="button" className={`bg-black border-purple border-2 text-white text-base font-semibold px-10 py-2 rounded-full hover:scale-125 ease-out duration-300`}>Download</button>
+		<button type="button" className={`bg-black border-purple border-2 text-white text-base font-semibold px-5 xs:px-10 py-2 rounded-full hover:scale-125 ease-out duration-300`}>Download</button>
 	</div>
 );
 
